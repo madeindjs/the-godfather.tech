55bfc936ff36e34ae42b3c10fba1f8a7bbaba1e4
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2022-01-31 00:55:33 +0100

    wip

--- frontend/src/views/GithubLogged.vue
+++ frontend/src/views/GithubLogged.vue
@@ -3,10 +3,17 @@
     <p>You have been logged with Github! Please wait...</p>
   </div>
 </template>
-<script scoped>
+
+<script>
 import axios from "axios";
-
 import { PROJECT_API_URL } from "../constants/project";
+import { useRoute } from "vue-router";
+export default {
+  setup() {
+    const route = useRoute();
 
-axios.post(`${PROJECT_API_URL}/github/auth`, { code: window.location.search.replace("?code=", "") });
+    axios.post(`${PROJECT_API_URL}/github/auth`, { code: route.query.code });
+    // TODO
+  },
+};
 </script>
