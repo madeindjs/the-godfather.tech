f0df46a17c38bbef99d60f112df346e80d120623
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2022-02-14 00:32:41 +0100

    improve faq

--- frontend/src/views/Faq.vue
+++ frontend/src/views/Faq.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="">
     <h1>Frequently Asked Questions</h1>
-    <details open id="#formula">
+
+    <h2>Global</h2>
+
+    <details id="#formula" :open="isFormulaOpen">
       <summary>How views are billed?</summary>
       <p>
         The price of each views is <strong>{{ PRICE_PER_VIEW_PER_STAR * 100 }} cents per star and per view</strong>.
@@ -30,6 +33,8 @@
       </table>
     </details>
 
+    <h2>For supporter</h2>
+
     <details>
       <summary>What are criteria?</summary>
       <p></p>
@@ -38,12 +43,46 @@
         <li>Number of stars</li>
       </ul>
     </details>
+    <details>
+      <summary>Can I suspend a campaign?</summary>
+      <p>
+        Yes, you can. You just need to hit "deactivate" on <router-link to="/campaigns">campaign list</router-link>.
+      </p>
+    </details>
+
+    <h2>As a developer</h2>
+
+    <details>
+      <summary>How I receive my money?</summary>
+      <p>TODO</p>
+    </details>
+    <details>
+      <summary>How to include tag?</summary>
+      <p>
+        <router-link to="/badge/new">Create a badge</router-link> and put it into your <code>README.md</code> file on
+        Github.
+      </p>
+    </details>
+    <details>
+      <summary>How to exclude some company to appear as sponsors?</summary>
+      <p>You can't do it yet.</p>
+    </details>
+
+    <h2>Other</h2>
+
+    <details>
+      <summary>Who are you?</summary>
+      <p>I'm <a href="https://rsseau.fr">Alexandre</a>. I'm a french software engineer passionate about Open-Source.</p>
+    </details>
   </div>
 </template>
 <script setup>
 import { formatMoney } from "../utils/formatter";
 import { calculatePrice } from "../utils/price";
 import { PRICE_PER_VIEW_PER_STAR } from "../constants/price";
+import { computed } from "@vue/runtime-core";
+
+const isFormulaOpen = computed(() => window.location.href.endsWith("formula"));
 
 const starsExamples = [1000, 10000, 30000, 50000];
 </script>
