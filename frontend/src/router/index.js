bd93764160263c1a5eae7c0738f0c1a500f1dd7e
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2022-03-22 00:13:01 +0100

    feat: add title per page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,5 @@
 // @ts-check
+import { nextTick } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 import { toastStore } from "../store/ToastStore";
 import { userStore } from "../store/UserStore";
@@ -16,33 +17,39 @@ const routes = [
   {
     path: "/",
     name: "Home",
+    meta: { title: "The-Godfather.tech" },
     component: Home,
   },
   {
     path: "/account",
     name: "Account",
+    meta: { title: "The-Godfather.tech" },
     component: () => import(/* webpackChunkName: "account" */ "../views/Account.vue"),
     beforeEnter: loginGuard,
   },
   {
     path: "/faq",
     name: "FAQ",
+    meta: { title: "FAQ" },
     component: () => import(/* webpackChunkName: "faq" */ "../views/Faq.vue"),
   },
   {
     path: "/badge/new",
     name: "/badge/new",
+    meta: { title: "New badge" },
     component: () => import(/* webpackChunkName: "create-tag" */ "../views/CreateImage.vue"),
   },
   {
     path: "/campaigns",
     name: "Campaigns",
+    meta: { title: "My campaigns" },
     component: () => import(/* webpackChunkName: "campaigns" */ "../views/Campaigns.vue"),
     beforeEnter: loginGuard,
   },
   {
     path: "/campaigns/new",
     name: "CampaignForm",
+    meta: { title: "New campaign" },
     component: () => import(/* webpackChunkName: "campaigns" */ "../views/CampaignForm.vue"),
     beforeEnter: loginGuard,
   },
@@ -50,17 +57,20 @@ const routes = [
     path: "/campaign/:id",
     name: "Campaign",
     props: true,
+    meta: { title: "My campaign" },
     component: () => import(/* webpackChunkName: "campaign" */ "../views/Campaign.vue"),
     beforeEnter: loginGuard,
   },
   {
     path: "/about",
     name: "About",
+    meta: { title: "About" },
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
     path: "/github-logged",
     name: "GithubLogged",
+    meta: { title: "Logged!" },
     component: () => import(/* webpackChunkName: "github-logged" */ "../views/GithubLogged.vue"),
   },
 ];
@@ -68,6 +78,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.afterEach((to) => {
+  nextTick(() => {
+    let title = "The-Godfather.tech";
+
+    if (to.meta.title) {
+      title = `${to.meta.title} | ${title}`;
+    }
+
+    document.title = title;
+  });
 });
 
 export default router;
